eba4ffcb5034a56fc2f831646a07598bee2cbd31
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-03-21 15:48:04 +0100

    Reading selected file content.

--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,28 @@
 'use strict';
 
+var fs = require('fs');
+
 angular.module('mora')
   .controller('MainCtrl', function ($scope) {
-        console.log("logged");
+
+
+    $scope.chooseFile =
+    function (name) {
+        console.log(name);
+        var chooser = document.querySelector(name);
+        chooser.addEventListener("change", function(evt) {
+            console.log(this.value);
+            fs.readFile(this.value, 'utf8', function (err, data) {
+                if (err)
+                {
+                    throw err;
+                }
+                $scope.$apply(function () {
+                    $scope.text = data;
+                });
+            });
+        }, false);
+
+        chooser.click();
+    };
   });
